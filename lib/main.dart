a410cdf5d5a37af2f45d1422e99181a7f25e8db4
RandyKdev <rkwalar@gmail.com>
2021-02-04 22:29:17 +0100

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,16 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       theme: ThemeData(primaryColor: Colors.blue),
-      home: SnackBarScreen(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text("SnackBar Example"),
+      ),
+        body: SnackBarScreen(),
+      ),
       title: 'SnackBar Example',
     );
   }
--- lib/screens/snackBar.dart
+++ lib/screens/snackBar.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 class SnackBarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("SnackBar Example"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(
-            "Open Snackbar",
-            style: TextStyle(
-              color: Colors.white,
-            ),
+    return Center(
+      child: RaisedButton(
+        child: Text(
+          "Open Snackbar",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          color: Colors.blue,
-          onPressed: () {},
         ),
+        color: Colors.blue,
+        onPressed: () {
+          final snack = SnackBar(
+            elevation: 5,
+            content: SizedBox(
+              height: 50,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("This is a Snack"),
+              ),
+            ),
+            action: SnackBarAction(
+              label: "OK",
+              onPressed: () {},
+            ),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        },
       ),
     );
   }
